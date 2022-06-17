6b75ba245f87ea0acc401a91f5521ab734066238
Gialon Kasha <gialonkasha@gmail.com>
2022-06-17 15:35:36 -0700

    Made some progress on Configurator, implemented callback function and updated the package.xml and CMake files

--- rosplan_agent_interface/include/rosplan_agent_interface/Configurator.h
+++ rosplan_agent_interface/include/rosplan_agent_interface/Configurator.h
@@ -6,25 +6,36 @@
 #ifndef CMU_CONFIGURATOR_H_
 #define CMU_CONFIGURATOR_H_
 
+#include <iostream>
+#include <string>
+#include <chrono>
+
 #include "ros/ros.h"
+#include "rosplan_dispatch_msgs/ActionFeedback.h"
 
 namespace KCL_rosplan
 {
     class Configurator
     {
     private:
-        std::string domain_file_;
+        std::string domain_path_;
         std::string problem_template_dir_;
+
+        std::double mission_start_time_;
+        rosplan_dispatch_msgs::ActionDispatch current_goal_;
+
+        ros::NodeHandle* node_handle_;
 
         void getCurrentState();
         void genProblemFile();
-        void configureExec();
+
+        void configure();
 
     public:
         Configurator(ros::NodeHandle& nh);
         ~Configurator();
         
-        void execute();
+        void goalRequestCallback();
     };
 }
 
---
+++ rosplan_agent_interface/src/Configurator.cpp
@@ -0,0 +1,64 @@
+#include <rosplan_agent_interface/Configurator.h>
+
+namespace KCL_rosplan {
+
+    Configurator::Configurator(ros::NodeHandle& nh, 
+                               std:string dp, 
+                               std::string ptdp) {
+        node_handle_ = nh;
+        domain_path_ = dp;
+        problem_template_dir_ = ptdp;
+    }
+
+    Configurator::~Configurator() {
+
+    }
+
+    void Configurator::getCurrentState() {
+
+    }
+
+    void Configurator::genProblemFile() {
+
+    }
+
+    bool Configurator::configure() {
+
+    }
+
+    void Configurator::goalRequestCallback(const rosplan_dispatch_msgs::ActionDispatch msg) {
+        ROS_INFO("KCL: (%s) Goal received.", ros::this_node::getName().c_str());
+        
+        mission_start_time = ros::WallTime::now.toSec();
+        current_goal_ = msg;
+
+        Configurator::configure();
+    }
+
+} // close namespace
+
+    int main(int argc, char **argv) {
+        
+        auto start = std::chrono::system_clock::now().time_since_epoch();
+        std::string name = "rosplan_agent_" + std::to_string(start.count());
+        
+        ros::init(argc, argv, name);
+        ros::NodeHandle nh("~");
+
+        // Get Configurator Info
+        std::string domain_path, ptd_path;
+        nh.param("domain_path", domain_path, domain_path);
+        nh.param("problem_template_dir_path", ptd_path, ptd_path);
+
+        KCL_rosplan::Configurator config(nh, domain_path, ptd_path);
+        
+        // Subscribe to receive goals
+        std::string goalRequestTopic = "goalRequest";
+        nh.getParam("goal_request_topic", goalRequestTopic);
+        ros::Subscriber goal_sub = nh.subscribe(goalRequestTopic, 1000, &KCL_rosplan::Configurator::goalRequestCallback, &config);
+
+        ROS_INFO("KCL: (%s) Ready to receive", ros::this_node::getName().c_str());
+        ros::spin();
+        
+        return 0;
+    }
